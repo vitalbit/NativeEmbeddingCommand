bcf25b119aebf987b5854ccecce2b3443fee36ca
Vitaly Ponteley <vitaly.ponteley@innowise-group.com>
2023-11-24 16:04:06 +0100

    Set maui context nullable

--- NativeEmbeddingCommand/Platforms/Android/MainActivity.cs
+++ NativeEmbeddingCommand/Platforms/Android/MainActivity.cs
@@ -11,7 +11,7 @@ namespace NativeEmbeddingCommand
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : AppCompatActivity
     {
-        MauiContext _mauiContext;
+        MauiContext? _mauiContext;
 
         protected override void OnCreate(Bundle? savedInstanceState)
         {
